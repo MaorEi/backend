e125ea102baf8afef3746a76d1d41f14a9d27acd
MaorEi <maor.eitan.me@gmail.com>
2024-08-05 01:52:14 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'django',
       script: 'demoproject/manage.py',
-      args: 'runserver',
+      args: 'runserver 0.0.0.0:8000',
       instances: 1,
       autorestart: true,
       watch: false,
